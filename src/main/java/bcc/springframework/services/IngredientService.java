8a6f51c4bf3e32c5ad342f7bc148a8039851c500
bryan2459 <mvs390@gmail.com>
2021-09-13 09:58:36 +1000

    202 View Ingredient, 203 Show Ingredient

---
+++ src/main/java/bcc/springframework/services/IngredientService.java
@@ -0,0 +1,11 @@
+package bcc.springframework.services;
+
+import bcc.springframework.commands.IngredientCommand;
+
+/**
+ * Created by jt on 6/27/17.
+ */
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+}
