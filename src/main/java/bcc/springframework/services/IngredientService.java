637213595e57b4d33a8f4db738adc389a05e445d
bryan2459 <mvs390@gmail.com>
2021-09-14 17:33:03 +1000

    204 Update Ingredient

--- src/main/java/bcc/springframework/services/IngredientService.java
+++ src/main/java/bcc/springframework/services/IngredientService.java
@@ -8,4 +8,6 @@ import bcc.springframework.commands.IngredientCommand;
 public interface IngredientService {
 
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
+
 }
---
+++ src/main/java/bcc/springframework/services/UnitOfMeasureService.java
@@ -0,0 +1,13 @@
+package bcc.springframework.services;
+
+import bcc.springframework.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/28/17.
+ */
+public interface UnitOfMeasureService {
+
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
